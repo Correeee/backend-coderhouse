37c6490e32825031cdb6a660ddf86fc1c7ed2394
elcorre <correa755@hotmail.com>
2023-07-26 15:51:59 -0300

    Filtro por rol

--- src/routes/usersRouterMongoose.js
+++ src/routes/usersRouterMongoose.js
@@ -10,15 +10,16 @@ const router = Router()
 
 /* -------------------------------- PASSPORT -------------------------------- */
 
-router.post('/register', passport.authenticate('register'), registerResponse)
-router.post('/login', passport.authenticate('login'), loginResponse)
-router.post('/logout', logoutController)
-router.get('/profile', profileInfoController)
+// router.post('/register', passport.authenticate('register'), registerResponse)
+// router.post('/login', passport.authenticate('login'), loginResponse)
+// router.post('/logout', logoutController)
+// router.get('/profile', profileInfoController)
 
-router.get('/registerGithub', passport.authenticate('githubPassport', { scope: ['user:email'] }))
-router.get('/profileGithub', passport.authenticate('githubPassport', { scope: ['user:email'] }), githubResponse)
+// router.get('/registerGithub', passport.authenticate('githubPassport', { scope: ['user:email'] }))
+// router.get('/profileGithub', passport.authenticate('githubPassport', { scope: ['user:email'] }), githubResponse)
 
 /* -------------------------- JWT - JSON WEB TOKEN -------------------------- */
+/* ---------------------------- USAR ESTAS RUTAS ---------------------------- */
 
 router.post('/registerJWT', createUserController)
 router.post('/loginJWT', loginUserController)
